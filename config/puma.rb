43ce0918f77fdbdede5db0db495bc4b0665cadfa
Jason Swett <jason@benfranklinlabs.com>
2020-01-22 10:17:17 -0500

    Add EB config to puma.rb.

--- config/puma.rb
+++ config/puma.rb
@@ -36,3 +36,7 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+# For Elastic Beanstalk
+bind "unix:///var/run/puma/my_app.sock"
+pidfile "/var/run/puma/my_app.sock"
